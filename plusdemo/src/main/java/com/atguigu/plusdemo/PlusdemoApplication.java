d2d0a31077f1d54d4fe1593d67219a4818820160
zhangsan <zhangsan@163.com>
2021-10-23 13:50:33 +0800

    Initial commit

---
+++ plusdemo/src/main/java/com/atguigu/plusdemo/PlusdemoApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.plusdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlusdemoApplication.class, args);
+    }
+
+}
